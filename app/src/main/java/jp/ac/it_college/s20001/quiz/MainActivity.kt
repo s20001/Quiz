25119f197746117b131096211bda8139541f8145
s20001 <s20001@std.it-college.ac.jp>
2021-08-30 23:08:12 +0900

    2

--- app/src/main/java/jp/ac/it_college/s20001/quiz/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/s20001/quiz/MainActivity.kt
@@ -1,11 +1,20 @@
 package jp.ac.it_college.s20001.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.s20001.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/jp/ac/it_college/s20001/quiz/SubActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.s20001.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
